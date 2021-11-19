146144028187dfeb60c1220e0656227d210ae7ef
isissi <isissi_525@hotmail.com>
2021-11-18 21:13:22 -0700

    initial push

---
+++ iss.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const fetchMyIP = function (callback) {
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    let IP = JSON.parse(body).ip;
+
+    callback(null, IP);
+  });
+};
+
+module.exports = { fetchMyIP };
