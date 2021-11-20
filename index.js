214d16812103bb44fa293dc505ce3f6f233b4bca
isissi <isissi_525@hotmail.com>
2021-11-19 19:11:48 -0700

    Added nextISSTimesForMyLocation

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+const {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+} = require("./iss");
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP:", ip);
+// });
+
+// fetchCoordsByIP("50.93.12.22",(error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned Geo Coordinates:", coords);
+// })
+
+// fetchISSFlyOverTimes({ latitude: 53.4179, longitude: -113.5785 }, (error, times) => {
+//   if (error) {
+//     console.log("Invalid latitude/longitude: ", error);
+//     return;
+//   }
+
+//   console.log("Response:", times);
+// })
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
 
-  console.log("It worked! Returned IP:", ip);
+  formatedTime(passTimes);
 });
 
-fetchCoordsByIP("50.93.12.22",(error, coords) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+const formatedTime = function (passTimes) {
+  for (const time of passTimes) {
+    const date = new Date(0);
+    date.setUTCSeconds(time.risetime);
+
+    console.log(`Next pass at ${date} for ${time.duration} seconds!`);
   }
-
-  console.log("It worked! Returned Geo Coordinates:", coords);
-})
-
-fetchISSFlyOverTimes({ latitude: 53.4179, longitude: -113.5785 }, (error, riseTime) => {
-  if (error) {
-    console.log("Invalid latitude/longitude: ", error);
-    return;
-  }
-
-  console.log("Response:", riseTime);
-})
+};
--- iss.js
+++ iss.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-const fetchMyIP = function(callback) {
+const fetchMyIP = function (callback) {
   request("https://api.ipify.org?format=json", (error, response, body) => {
     if (error) {
       return callback(error, null);
@@ -18,7 +18,7 @@ const fetchMyIP = function(callback) {
   });
 };
 
-const fetchCoordsByIP = function(ip, callback) {
+const fetchCoordsByIP = function (ip, callback) {
   request(
     "https://api.freegeoip.app/json/?apikey=db333400-48f0-11ec-af67-e7b1abfbdab5",
     (error, response, body) => {
@@ -40,26 +40,50 @@ const fetchCoordsByIP = function(ip, callback) {
   );
 };
 
-const fetchISSFlyOverTimes = function(coords, callback) {
+const fetchISSFlyOverTimes = function (coords, callback) {
   const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
 
-  request(
-    url,
-    (error, response, body) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const times = JSON.parse(body).response;
+    callback(null, times);
+  });
+};
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, coords) => {
       if (error) {
         return callback(error, null);
       }
 
-      if (response.statusCode !== 200) {
-        const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-        callback(Error(msg), null);
-        return;
-      }
+      fetchISSFlyOverTimes(coords, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
 
-      const riseTime = JSON.parse(body).response;
-      callback(null,riseTime);
-    }
-  );
+        callback(null, nextPasses);
+      });
+    });
+  });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation,
+};
